968ce61e895570c85b84dde3e8de612eb621cfd8
Tejas Panchal <nitingrover@live.com>
2023-12-28 12:11:39 +0530

    main

--- components/About/About1.js
+++ components/About/About1.js
@@ -53,8 +53,8 @@ const About1 = ({ clientHeight }) => {
           className="font-medium text-[2.70rem] md:text-6xl lg:text-[4rem] text-center"
         >
           <span className="about-1 leading-tight">
-            I&apos;m a passionate "slave" who&apos;s focused on finding
-            abcdefgh and performant apps.{" "}
+            I&apos;m a passionate Engineer who&apos;s focused on building
+            scalable and performant apps.{" "}
           </span>
           <span className="about-2 leading-tight">
             I take responsibility to craft a good user experience using modern
--- constants.js
+++ constants.js
@@ -200,4 +200,4 @@ export const WORK = [
   },
 ];
 
-export const GTAG = "G-5HCTL2TJ5W";
+// export const GTAG = "G-5HCTL2TJ5W";
